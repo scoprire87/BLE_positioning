2654e7e0101e78bf4476bca57436fcdc3d8c577d
scoprire87 <acintolesi6@gmail.com>
2026-04-06 23:26:16 +0200

    Update constants for BLE Radar tests

--- tests/const.py
+++ tests/const.py
@@ -1,36 +1,32 @@
-"""Constants for Bermuda BLE Trilateration tests."""
+"""Constants for BLE Radar tests."""
 
 from __future__ import annotations
 
-import custom_components.bermuda.const
-
-# from custom_components.bermuda.const import CONF_DEVICES
-# from custom_components.bermuda.const import CONF_MAX_RADIUS
-
+import custom_components.ble_radar.const
 
 MOCK_OPTIONS = {
-    custom_components.bermuda.const.CONF_MAX_RADIUS: 20.0,
-    custom_components.bermuda.const.CONF_MAX_VELOCITY: 3.0,
-    custom_components.bermuda.const.CONF_DEVTRACK_TIMEOUT: 30,
-    custom_components.bermuda.const.CONF_UPDATE_INTERVAL: 10.0,
-    custom_components.bermuda.const.CONF_SMOOTHING_SAMPLES: 20,
-    custom_components.bermuda.const.CONF_ATTENUATION: 3.0,
-    custom_components.bermuda.const.CONF_REF_POWER: -55.0,
-    custom_components.bermuda.const.CONF_DEVICES: [],  # ["EE:E8:37:9F:6B:54"],
+    custom_components.ble_radar.const.CONF_MAX_RADIUS: 20.0,
+    custom_components.ble_radar.const.CONF_MAX_VELOCITY: 3.0,
+    custom_components.ble_radar.const.CONF_DEVTRACK_TIMEOUT: 30,
+    custom_components.ble_radar.const.CONF_UPDATE_INTERVAL: 10.0,
+    custom_components.ble_radar.const.CONF_SMOOTHING_SAMPLES: 20,
+    custom_components.ble_radar.const.CONF_ATTENUATION: 3.0,
+    custom_components.ble_radar.const.CONF_REF_POWER: -55.0,
+    custom_components.ble_radar.const.CONF_DEVICES: [],  # ["EE:E8:37:9F:6B:54"],
 }
 
 MOCK_OPTIONS_GLOBALS = {
-    custom_components.bermuda.const.CONF_MAX_RADIUS: 20.0,
-    custom_components.bermuda.const.CONF_MAX_VELOCITY: 3.0,
-    custom_components.bermuda.const.CONF_DEVTRACK_TIMEOUT: 30,
-    custom_components.bermuda.const.CONF_UPDATE_INTERVAL: 10.0,
-    custom_components.bermuda.const.CONF_SMOOTHING_SAMPLES: 20,
-    custom_components.bermuda.const.CONF_ATTENUATION: 3.0,
-    custom_components.bermuda.const.CONF_REF_POWER: -55.0,
+    custom_components.ble_radar.const.CONF_MAX_RADIUS: 20.0,
+    custom_components.ble_radar.const.CONF_MAX_VELOCITY: 3.0,
+    custom_components.ble_radar.const.CONF_DEVTRACK_TIMEOUT: 30,
+    custom_components.ble_radar.const.CONF_UPDATE_INTERVAL: 10.0,
+    custom_components.ble_radar.const.CONF_SMOOTHING_SAMPLES: 20,
+    custom_components.ble_radar.const.CONF_ATTENUATION: 3.0,
+    custom_components.ble_radar.const.CONF_REF_POWER: -55.0,
 }
 
 MOCK_OPTIONS_DEVICES = {
-    custom_components.bermuda.const.CONF_DEVICES: [],  # ["EE:E8:37:9F:6B:54"],
+    custom_components.ble_radar.const.CONF_DEVICES: [],  # ["EE:E8:37:9F:6B:54"],
 }
 
 MOCK_CONFIG = {"source": "user"}
